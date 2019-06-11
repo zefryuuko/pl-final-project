1cf81faa911cd7f156179a87fc9011ea78255eba
zefryuuko <titotobing0511@gmail.com>
2019-06-11 23:13:59 +0700

    Added HTML support for messages

--- src/zefryuuko/chat/client/gui/MessageContainer.java
+++ src/zefryuuko/chat/client/gui/MessageContainer.java
@@ -1,14 +1,18 @@
 package zefryuuko.chat.client.gui;
 
 import javax.swing.*;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
 import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 public class MessageContainer extends JPanel
 {
     private GridBagConstraints c = new GridBagConstraints();
     private JLabel lblUsername = new JLabel();
     private JTextPane txtMessage = new JTextPane();
-    private String txtMessageCSS = "body{color:#ffffff;font-family:'Helvetica Neue', sans-serif;font-size:10px;}";
+    private String txtMessageCSS = "body{color:#ffffff;font-family:'Helvetica Neue',sans-serif;font-size:10px;}a{color:#ffffff;}.imgview{height:200px;width:300px;}.container{background:#3d4147;padding:5px 10px 7px 10px;margin-top:5px;width:100%;color:#e5e5e5;}.container-alt{background:#3d4147;padding:5px 6px 7px 6px;margin-top:5px;color:#e5e5e5;}.subheading{font-size:11px;}";
 
     public MessageContainer(String username, String message)
     {
@@ -19,11 +23,12 @@ public class MessageContainer extends JPanel
 
         // Object properties
         lblUsername.setText(username);
-        lblUsername.setForeground(Color.WHITE);
+        lblUsername.setForeground(getNameColor(username));
         lblUsername.setFont(new Font("Helvetica Neue", Font.BOLD, 15));
         txtMessage.setEditable(false);
         txtMessage.setContentType("text/html");
-        txtMessage.setText("<html><style>" + txtMessageCSS + "</style><body>" + message + "</body></html>");
+        txtMessage.setText("<html><style>" + txtMessageCSS + "</style><body>" + processRichText(message) + "</body></html>");
+        txtMessage.addHyperlinkListener(new txtMessageHyperlinkListener());
         txtMessage.setForeground(new Color(215, 216, 217));
         txtMessage.setBackground(new Color(54, 57, 62));
         txtMessage.setBorder(null);
@@ -37,6 +42,85 @@ public class MessageContainer extends JPanel
         this.add(lblUsername, c); c.gridy++;
         this.c.insets = new Insets(2, 0, 0, 0);
         this.add(txtMessage, c);
+    }
 
+    private Color getNameColor(String name)
+    {
+        switch (name)
+        {
+            case "Git": return Color.PINK;
+            case "Notification": return Color.CYAN;
+            default: return Color.WHITE;
+        }
+    }
+
+    private String processRichText(String message)
+    {
+        // Escape symbols used by HTML
+        if (!message.startsWith("[noescape]") && !message.endsWith("[/noescape]"))
+        {
+            message = message.replaceAll("<", "&lt;");
+            message = message.replaceAll(">", "&gt;");
+        }
+        else
+        {
+            message = message.replace("[noescape]", "");
+            message = message.replace("[/noescape]", "");
+        }
+
+        String output = "";
+        String[] tokenizedMessage = message.split(" ");
+
+
+
+        for (int i = 0; i < tokenizedMessage.length; i++)
+        {
+            String token = tokenizedMessage[i];
+            if (isURL(token) && isImageURL(token))
+                tokenizedMessage[i] = String.format("<a href='%s'>%s</a><br><div class='container-alt' width=312px><div class='imgview' style='background-image:url(\"%s\");'></div></div>", token, token, token);
+            else if (isURL(token))
+                tokenizedMessage[i] = String.format("<a href='%s'>%s</a>", token, token);
+        }
+
+        for (String token : tokenizedMessage)
+        {
+            output = output + " " + token;
+        }
+
+        return output.substring(1);
+    }
+
+    private boolean isURL(String text)
+    {
+        text = text.toLowerCase();
+        if (text.startsWith("http://") || text.startsWith("https://")) return true;
+        return false;
+    }
+
+    private boolean isImageURL(String text)
+    {
+        text = text.toLowerCase();
+        if (text.endsWith(".png") || text.endsWith(".jpg") || text.endsWith(".gif")) return true;
+        return false;
+    }
+
+    private class txtMessageHyperlinkListener implements HyperlinkListener
+    {
+        @Override
+        public void hyperlinkUpdate(HyperlinkEvent e)
+        {
+            if (HyperlinkEvent.EventType.ACTIVATED.equals(e.getEventType()))
+            {
+                try
+                {
+                    Desktop desktop = Desktop.getDesktop();
+                    desktop.browse(e.getURL().toURI());
+                }
+                catch (IOException | URISyntaxException ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        }
     }
 }
