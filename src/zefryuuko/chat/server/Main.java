e35286851cbf588cb1ef0416db6160997023018e
zefryuuko <titotobing0511@gmail.com>
2019-06-11 17:43:31 +0700

    Added git notification routine

--- src/zefryuuko/chat/server/Main.java
+++ src/zefryuuko/chat/server/Main.java
@@ -1,6 +1,7 @@
 package zefryuuko.chat.server;
 
 import zefryuuko.chat.lib.Git;
+import zefryuuko.chat.lib.Routine;
 import zefryuuko.chat.lib.Utilities;
 
 import java.util.HashMap;
@@ -12,14 +13,19 @@ public class Main
     private static String serverPassword = "test";
     private static String repoAddress = "https://github.com/zefryuuko/useless-repository.git";
     private static Git git;
+    private static Routine gitNotificationRoutine;
 
     public static void main(String[] args)
     {
         Utilities.makeDir("appdata/repofiles");
-        git = new Git(repoAddress);
+
         connectedUsers = new HashMap();
         server = new Server(5550);
         server.start();
+
+        git = new Git(repoAddress);
+        gitNotificationRoutine = new Routine(new GitNotificationRoutine(), 10000);
+        gitNotificationRoutine.start();
     }
 
     public static Server getServer()
