4b19aba2d46b5c04a691527255584e0b9ff63e99
zefryuuko <titotobing0511@gmail.com>
2019-06-15 15:54:26 +0700

    Added serverDescription field

--- src/zefryuuko/chat/commdata/ServerPropertiesData.java
+++ src/zefryuuko/chat/commdata/ServerPropertiesData.java
@@ -3,13 +3,15 @@ package zefryuuko.chat.commdata;
 public class ServerPropertiesData extends CommData
 {
     private final String serverName;
+    private final String serverDescription;
     private final boolean hasGit;
     private final String gitAddress;
 
-    public ServerPropertiesData(String serverName, boolean hasGit, String gitAddress)
+    public ServerPropertiesData(String serverName, String serverDescription, boolean hasGit, String gitAddress)
     {
         super("ServerPropertiesData");
         this.serverName = serverName;
+        this.serverDescription = serverDescription;
         this.hasGit = hasGit;
         this.gitAddress = gitAddress;
     }
