cbef50047d9b6abb1c6dc7e46d5de44ce8952e81
zefryuuko <titotobing0511@gmail.com>
2019-06-15 17:27:38 +0700

    Added messages saving method

--- src/zefryuuko/chat/server/MessagesContainer.java
+++ src/zefryuuko/chat/server/MessagesContainer.java
@@ -1,11 +1,15 @@
 package zefryuuko.chat.server;
 
 import zefryuuko.chat.commdata.ChatData;
+import zefryuuko.chat.lib.Utilities;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.LinkedList;
 
-public class MessagesContainer implements Serializable
+public class MessagesContainer
 {
     private final int limit;
     private LinkedList<ChatData> messages;
@@ -20,10 +24,26 @@ public class MessagesContainer implements Serializable
     {
         messages.addLast(chatData);
         if (messages.size() > limit) messages.removeFirst();
+        saveToFile();
     }
 
     public LinkedList<ChatData> getMessages()
     {
         return messages;
+    }
+
+    private void saveToFile()
+    {
+        try
+        {
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("appdata/saved-messages.msgcontainer"));
+            bufferedWriter.write(Utilities.objSerialize(this.messages));
+            bufferedWriter.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
