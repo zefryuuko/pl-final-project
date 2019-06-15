cf04d5ecd55eaf1fea2e1430d32b34a82b11e61d
zefryuuko <titotobing0511@gmail.com>
2019-06-15 16:30:44 +0700

    Added ScrollablePanel

---
+++ src/zefryuuko/chat/client/gui/ScrollablePanel.java
@@ -0,0 +1,30 @@
+package zefryuuko.chat.client.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ScrollablePanel extends JPanel implements Scrollable
+{
+    public Dimension getPreferredScrollableViewportSize() {
+        return getPreferredSize();
+    }
+
+    public int getScrollableUnitIncrement(Rectangle visibleRect, int orientation, int direction) {
+        return 10;
+    }
+
+    public int getScrollableBlockIncrement(Rectangle visibleRect, int orientation, int direction) {
+        return ((orientation == SwingConstants.VERTICAL) ? visibleRect.height : visibleRect.width) - 10;
+    }
+
+    public boolean getScrollableTracksViewportWidth() {
+        return true;
+    }
+
+    public boolean getScrollableTracksViewportHeight() {
+        return false;
+    }
+}
+
+// Referenced from
+// https://stackoverflow.com/questions/2716274/jscrollpane-needs-to-shrink-its-width
