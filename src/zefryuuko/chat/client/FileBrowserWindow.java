ad7b4bfdf47e72340ae7178c95cdfb90e6694386
zefryuuko <titotobing0511@gmail.com>
2019-06-20 21:32:27 +0700

    Added text viewer navbar

--- src/zefryuuko/chat/client/FileBrowserWindow.java
+++ src/zefryuuko/chat/client/FileBrowserWindow.java
@@ -13,18 +13,24 @@ public class FileBrowserWindow extends JFrame
 {
     private final String baseDir;
     private String currentSelectedDir;
+    private String dirFromRoot;
     private JPanel pnlMain = new JPanel();
     private JPanel pnlBrowse = new JPanel();
     private JPanel pnlNavigation = new JPanel();
+    private JPanel pnlViewer = new JPanel();
+    private JPanel pnlFileActions = new JPanel();
     private JScrollPane spaneFiles = new JScrollPane();
     private JButton btnBack = new JButton();
     private JList lstFiles = new JList();
+    private JLabel lblFilePath = new JLabel();
+    private JButton btnDiscuss = new JButton();
     private LineNumberTextArea txtPreview = new LineNumberTextArea();
     private GridBagConstraints c = new GridBagConstraints();
 
     public FileBrowserWindow(String baseDir)
     {
         this.baseDir = baseDir;
+        this.dirFromRoot = "/";
         populateFileList(baseDir);
 
         // Window properties
@@ -46,6 +52,15 @@ public class FileBrowserWindow extends JFrame
         lstFiles.setBackground(new Color(59, 63, 68));
         lstFiles.setForeground(Color.WHITE);
         spaneFiles.getViewport().setOpaque(false);
+        pnlViewer.setLayout(new GridBagLayout());
+        pnlViewer.setBackground(new Color(59, 63, 68));
+        pnlNavigation.setMaximumSize(new Dimension(20, 30));
+        pnlNavigation.setPreferredSize(new Dimension(20, 30));
+        pnlNavigation.setMinimumSize(new Dimension(20, 30));
+        pnlFileActions.setBackground(new Color(44, 47, 51));
+        pnlFileActions.setLayout(new GridBagLayout());
+        btnDiscuss.setText("Discuss selected");
+        lblFilePath.setForeground(Color.WHITE);
         txtPreview.setTextAreaBackground(new Color(44, 47, 51));
         txtPreview.setTextAreaForeground(Color.WHITE);
         txtPreview.setLineNumberingBackground(new Color(59, 63, 68));
@@ -57,7 +72,6 @@ public class FileBrowserWindow extends JFrame
 
         // Panel objects
         pnlNavigation.add(btnBack);
-
         c.gridx = c.gridy = 0;
         c.weightx = 1;
         c.weighty = 0;
@@ -66,13 +80,31 @@ public class FileBrowserWindow extends JFrame
         c.weightx = c.weighty = 1;
         pnlBrowse.add(spaneFiles, c);
 
+        c.gridx = c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 0;
+        c.fill = GridBagConstraints.BOTH;
+        pnlFileActions.add(lblFilePath, c); c.gridx++;
+        c.weightx = 0;
+        c.fill = GridBagConstraints.NONE;
+        pnlFileActions.add(btnDiscuss, c);
+
+        c.gridx = c.gridy = 0;
+        c.weightx = 1;
+        c.weighty = 0;
+        c.fill = GridBagConstraints.BOTH;
+        pnlViewer.add(pnlFileActions, c); c.gridy++;
+        c.weighty++;
+        pnlViewer.add(txtPreview, c);
+
+
         c.insets = new Insets(10, 10, 10, 5);
         c.gridx = c.gridy = 0;
         c.weightx = c.weighty = 1;
         c.fill = GridBagConstraints.BOTH;
         pnlMain.add(pnlBrowse, c); c.gridx++;
         c.insets = new Insets(10, 5, 10, 10);
-        pnlMain.add(txtPreview, c);
+        pnlMain.add(pnlViewer, c);
     }
 
     private void populateFileList(String path)
@@ -90,10 +122,19 @@ public class FileBrowserWindow extends JFrame
     private void goBack()
     {
         String[] currentDirSplit = currentSelectedDir.split("/");
+        String[] dirFromRootSplit = dirFromRoot.split("/");
         String nextDir = "";
+        String nextDirFromRoot = "";
+
         for (int i = 0; i < currentDirSplit.length - 1; i++)
             nextDir += currentDirSplit[i] + "/";
+
+        for (int i = 0; i < dirFromRootSplit.length - 1; i++)
+            nextDirFromRoot += dirFromRootSplit[i] + "/";
+
         currentSelectedDir = nextDir.substring(0, nextDir.length() - 1);
+        dirFromRoot = nextDirFromRoot.substring(0, nextDirFromRoot.length() - 1);
+
         populateFileList(currentSelectedDir);
         if (currentSelectedDir.equals(baseDir))
             btnBack.setEnabled(false);
@@ -108,6 +149,7 @@ public class FileBrowserWindow extends JFrame
                 if (Utilities.dirExists(currentSelectedDir + "/" + lstFiles.getSelectedValue()))
                 {
                     currentSelectedDir = currentSelectedDir + "/" + lstFiles.getSelectedValue();
+                    dirFromRoot = dirFromRoot + lstFiles.getSelectedValue() + "/";
                     populateFileList(currentSelectedDir);
                     btnBack.setEnabled(true);
                 }
@@ -120,6 +162,7 @@ public class FileBrowserWindow extends JFrame
                         String currentLine;
                         while ((currentLine = bufferedReader.readLine()) != null) fileContent += currentLine + "\n";
                         txtPreview.setText(fileContent.substring(0, fileContent.length() - 1));
+                        lblFilePath.setText(dirFromRoot + lstFiles.getSelectedValue());
                     }
                     catch (IOException e)
                     {
