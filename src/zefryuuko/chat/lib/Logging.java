bee4d8ebb98eb2527e3f85c7e5e187c48769b1dc
zefryuuko <titotobing0511@gmail.com>
2019-06-23 18:37:08 +0700

    Removed unnecessary methods

--- src/zefryuuko/chat/lib/Logging.java
+++ src/zefryuuko/chat/lib/Logging.java
@@ -3,7 +3,6 @@ package zefryuuko.chat.lib;
 public class Logging
 {
     private final String moduleName;
-    private boolean writeToFile = false;
 
     public Logging(String moduleName)
     {
@@ -14,21 +13,5 @@ public class Logging
     {
         String log = String.format("[%s] %s\n", this.moduleName, text);
         System.out.print(log);
-        if (writeToFile) appendToLog(log);
-    }
-
-    public void appendToLog(String text)
-    {
-
-    }
-
-    public boolean isWriteToFile()
-    {
-        return writeToFile;
-    }
-
-    public void setWriteToFile(boolean writeToFile)
-    {
-        this.writeToFile = writeToFile;
     }
 }
