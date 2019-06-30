474667709f840d19d986de77e80af7f0a61d8cee
zefryuuko <titotobing0511@gmail.com>
2019-06-30 22:04:19 +0700

    Deleted unused files

--- src/zefryuuko/chat/commdata/TextEditorDisconnectData.java
+++
@@ -1,9 +0,0 @@
-package zefryuuko.chat.commdata;
-
-public class TextEditorDisconnectData extends CommData
-{
-    public TextEditorDisconnectData()
-    {
-        super("TextEditorDisconnectData");
-    }
-}
